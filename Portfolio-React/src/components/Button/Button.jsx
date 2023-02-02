b63fcbf9ea0aa03f28bfa85173f93cfd9de4e132
Lukas <lukasdaugela197@gmail.com>
2023-02-02 08:22:18 +0200

    Installed classnames

--- Portfolio-React/src/components/Button/Button.jsx
+++ Portfolio-React/src/components/Button/Button.jsx
@@ -1,5 +1,10 @@
+import className from 'classnames/bind';
 import React from 'react';
 
+import styles from './Button.module.scss';
+
+const cn = className.bind(styles);
+
 export default function Button() {
-  return <div>Button</div>;
+  return <div className={cn('divas')}>Button</div>;
 }
