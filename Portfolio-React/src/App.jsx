23762b60db2adaf301e59495e1370c3d023d3e4a
Lukas <lukasdaugela197@gmail.com>
2023-02-03 09:58:16 +0200

    Created Nav component, added svg's

--- Portfolio-React/src/App.jsx
+++ Portfolio-React/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Button from './components/Button';
 import Footer from './components/Footer/Footer';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="app">
+      <Navigation />
       <Button>Download cv</Button>
       <Footer />
     </div>
