542655ebb4c06a452fe02b6d276a9e8da264b2f0
Lukas <lukasdaugela197@gmail.com>
2023-02-04 13:11:01 +0200

    Added Section-heading component

--- Portfolio-React/src/components/AboutSection/AboutSection.jsx
+++ Portfolio-React/src/components/AboutSection/AboutSection.jsx
@@ -1,6 +1,7 @@
 import className from 'classnames/bind';
 import React from 'react';
 
+import SectionsHeading from '../SectionsHeading';
 import styles from './AboutSection.module.scss';
 
 const cn = className.bind(styles);
@@ -8,7 +9,7 @@ const cn = className.bind(styles);
 export default function AboutSection() {
   return (
     <div className={cn('about')}>
-      <h2 className={cn('about__title')}>about me</h2>
+      <SectionsHeading>about me</SectionsHeading>
       <p className={cn('about__description')}>tekstas</p>
     </div>
   );
