10caf512db46a335372f8588f7d35be5b699581c
Lukas <lukasdaugela197@gmail.com>
2023-02-04 16:11:32 +0200

    Added text

--- Portfolio-React/src/components/AboutSection/AboutSection.jsx
+++ Portfolio-React/src/components/AboutSection/AboutSection.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 import SectionsHeading from '../SectionsHeading';
 import styles from './AboutSection.module.scss';
+import { text } from './text';
 
 const cn = className.bind(styles);
+
+const { description } = text;
 
 export default function AboutSection() {
   return (
     <div className={cn('about')}>
       <SectionsHeading>about me</SectionsHeading>
-      <p className={cn('about__description')}>tekstas</p>
+      <p className={cn('about__description')}>{description}</p>
     </div>
   );
 }
