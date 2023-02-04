10929b957f5f492e1b713daf3f2c994fabc4f69b
Lukas <lukasdaugela197@gmail.com>
2023-02-04 16:29:03 +0200

    Added Horizontal line

--- Portfolio-React/src/components/AboutSection/AboutSection.jsx
+++ Portfolio-React/src/components/AboutSection/AboutSection.jsx
@@ -1,6 +1,7 @@
 import className from 'classnames/bind';
 import React from 'react';
 
+import { HorizontalLine } from '../../../public/assets/svg';
 import SectionsHeading from '../SectionsHeading';
 import styles from './AboutSection.module.scss';
 import { text } from './text';
@@ -14,6 +15,7 @@ export default function AboutSection() {
     <div className={cn('about')}>
       <SectionsHeading>about me</SectionsHeading>
       <p className={cn('about__description')}>{description}</p>
+      <HorizontalLine />
     </div>
   );
 }
