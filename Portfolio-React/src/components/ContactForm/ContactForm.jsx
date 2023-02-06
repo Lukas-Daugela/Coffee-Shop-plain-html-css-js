5fb5dcdcf34e248ddbceff60b03d67a52a12e0e4
Lukas <lukasdaugela197@gmail.com>
2023-02-06 21:25:37 +0200

    Implemented Formik

--- Portfolio-React/src/components/ContactForm/ContactForm.jsx
+++ Portfolio-React/src/components/ContactForm/ContactForm.jsx
@@ -1,37 +1,69 @@
 import className from 'classnames/bind';
+import { Form, Formik } from 'formik';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef } from 'react';
 
+import Button from '../Button';
 import TextArea from '../TextArea';
 import TextInput from '../TextInput';
 import styles from './ContactForm.module.scss';
+import withValidation from './withValidation';
 
 const cn = className.bind(styles);
 
+const FieldTextInput = withValidation(TextInput);
+const FieldTextArea = withValidation(TextArea);
+
 export default function ContactForm({ texts }) {
+  const formikInnerRef = useRef();
+
+  const initialValues = {
+    firstName: '',
+    email: '',
+    phone: '',
+    message: '',
+  };
+
   const { firstNameText, phoneNumber, email, message } = texts;
 
+  const handleOnSubmit = (values) => {
+    const formData = new FormData();
+    Object.entries(values).forEach(([key, value]) => formData.append(key, value));
+  };
+
   return (
-    <form className={cn('contact')}>
-      <TextInput
-        name="firstName"
-        autoComplete="given-name"
-        placeholder={firstNameText.placeholder}
-        required
-      />
-      <TextInput
-        name="email"
-        autoComplete="email"
-        placeholder={email.placeholder}
-        required
-      />
-      <TextInput
-        name="phone"
-        autoComplete="phone-number"
-        placeholder={phoneNumber.placeholder}
-      />
-      <TextArea name="message" placeholder={message.placeholder} required />
-    </form>
+    <Formik
+      innerRef={formikInnerRef}
+      initialValues={initialValues}
+      onSubmit={handleOnSubmit}
+      // validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => (
+        <Form className={cn('contact-form')}>
+          <FieldTextInput
+            name="firstName"
+            autoComplete="given-name"
+            placeholder={firstNameText.placeholder}
+            required
+          />
+          <FieldTextInput
+            name="email"
+            autoComplete="email"
+            placeholder={email.placeholder}
+            required
+          />
+          <FieldTextInput
+            name="phone"
+            autoComplete="phone-number"
+            placeholder={phoneNumber.placeholder}
+          />
+          <FieldTextArea name="message" placeholder={message.placeholder} required />
+          <div className={cn('contact-form__button-wrapper')}>
+            <Button onClick={handleSubmit}>Send</Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
