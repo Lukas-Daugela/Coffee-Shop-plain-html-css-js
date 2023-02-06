2fa08974b46293af34c39e10f3e5add3c63c2b1f
Lukas <lukasdaugela197@gmail.com>
2023-02-06 18:11:35 +0200

    Added Text-area component

--- Portfolio-React/src/App.jsx
+++ Portfolio-React/src/App.jsx
@@ -5,6 +5,7 @@ import AboutSection from './components/AboutSection/AboutSection';
 import Hero from './components/Hero';
 import SectionPorfolio from './components/SectionPorfolio/SectionPorfolio';
 import SkillsSection from './components/SkillsSection';
+import TextArea from './components/TextArea/TextArea';
 import TextInput from './components/TextInput/TextInput';
 import PageLayout from './layouts/PageLayout/PageLayout';
 // import styles from './layouts/PageLayout/PageLayout.module.scss';
@@ -20,6 +21,13 @@ function App() {
         <SkillsSection />
         <SectionPorfolio />
         <TextInput
+          name="firstName"
+          autoComplete="given-name"
+          placeholder={'Add you name'}
+          required
+          error="Required"
+        />
+        <TextArea
           name="firstName"
           autoComplete="given-name"
           placeholder={'Add you name'}
