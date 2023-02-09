a4f8baaf26fd6ebc9ef1189ffdf30216eff24f99
Lukas <lukasdaugela197@gmail.com>
2023-02-09 09:02:05 +0200

    Removed animation from hero-button

--- src/components/HeroButton/HeroButton.jsx
+++ src/components/HeroButton/HeroButton.jsx
@@ -1,7 +1,6 @@
-import Aos from 'aos';
 import className from 'classnames/bind';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { HashLink } from 'react-router-hash-link';
 
 import styles from './HeroButton.module.scss';
@@ -11,32 +10,15 @@ const cn = className.bind(styles);
 export default function HeroButton({ children, hashLink, link }) {
   let button;
 
-  useEffect(() => {
-    Aos.init();
-  }, []);
-
   if (hashLink) {
     button = (
-      <HashLink
-        data-aos="fade-right"
-        data-aos-delay="100"
-        data-aos-once={true}
-        to={hashLink}
-        smooth
-      >
+      <HashLink to={hashLink} smooth>
         <button className={cn('btn')}>{children}</button>
       </HashLink>
     );
   } else {
     button = (
-      <a
-        data-aos="fade-right"
-        data-aos-delay="100"
-        data-aos-once={true}
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href={link} target="_blank" rel="noreferrer">
         <button className={cn('btn')}>{children}</button>
       </a>
     );
