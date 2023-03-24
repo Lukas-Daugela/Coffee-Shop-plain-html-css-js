9f008ac7c0814d0d26571e9719495eaa51721ac0
Lukas <lukasdaugela197@gmail.com>
2023-03-24 10:35:58 +0200

    Implemented Loader for page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import './sass/index.scss';
 
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import Loader from './components/Loader/Loader';
+const App = lazy(() => import('./App'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Suspense fallback={<Loader />}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>,
 );
